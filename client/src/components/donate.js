172c8362b9502e29305cb0e4cc0582d1baebde2f
Gld-Mx24483 <israeltoki7@gmail.com>
2024-02-23 14:04:14 +0100

    NEW

--- client/src/components/donate.js
+++ client/src/components/donate.js
@@ -12,21 +12,19 @@ const Donate = () => {
         <div className="left-half">
           <h3>Donate Now</h3>
           <h2>Thanks For The Results Achieved With You</h2>
-          <p>Write some paragraph text here...</p>
+          <p>We extend our heartfelt gratitude for your ongoing commitment and contributions. 
+            Thanks to your dedication, we anticipate even greater successes in the future.</p>
         </div>
         <div className="right-half">
           <form>
-            <label htmlFor="name">Your Name</label>
-            <input type="text" id="name" name="name" required />
+            <input placeholder="Your Name" type="text" id="name" name="name" required />
 
-            <label htmlFor="email">Your Email</label>
-            <input type="email" id="email" name="email" required />
+            <input placeholder="Your Email" type="email" id="email" name="email" required />
 
-            <label htmlFor="amount">Amount (NGN)</label>
-            <input type="number" id="amount" name="amount" required />
+            <input placeholder="Amount (NGN)" type="number" id="amount" name="amount" required />
 
             <button type="submit" className="donate-btn">
-              Donate Now <FontAwesomeIcon icon={faArrowRight} />
+              Donate Now  <i className='fas fa-arrow-right'></i>
             </button>
           </form>
         </div>
