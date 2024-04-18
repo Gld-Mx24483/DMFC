f9c69f755cb69623760fc1d42170f902df8cb8c3
Gld-Mx24483 <israeltoki7@gmail.com>
2024-04-18 14:28:12 +0100

    upper

--- client/src/components/blogs.js
+++ client/src/components/blogs.js
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import './blogs.css';
 
 const Blog = () => {
   const [content, setContent] = useState([]);
@@ -31,18 +32,18 @@ const Blog = () => {
   return (
     <div className="events-user-main-container">
         <div className="events-heading-container">
-        <h2 className="events-heading">Upcoming Events</h2>
+        <h2 className="events-heading">Latest Blogs</h2>
         <p className="events-subheading">
-          Discover the latest <span className="spanns">events</span> happening{' '}
-          <span className="spanns">soon!!!</span>
+          Explore our <span className="spanns">newest</span> blog posts{' '} <span className="spanns">today!!!</span>
         </p>
       </div>
       {selectedContent ? (
         <div className="selected-event-container">
             <div className="selected-event-image">
-            <img src={selectedContent.imagePath} alt={selectedContent.title} />
+            <img src={selectedContent.imagePath} />
+            <video className="event-card-image" controls src={selectedContent.videoPath}></video>
           </div>
-          <div className='selected-event-content'>
+          <div className='selected-event-content contentss'>
           <h2>{selectedContent.title}</h2>
           <p>{selectedContent.dateTime} - {selectedContent.uploadTime}</p>
           <p>{selectedContent.fullName}</p>
@@ -56,9 +57,8 @@ const Blog = () => {
         <div className="event-cards-container">
           {content.map((item, index) => (
             <div key={index} className="event-card" onClick={() => handleContentClick(item)}>
-              {item.imagePath && (
-                <img src={item.imagePath} alt={item.title} className="event-card-image" />
-              )}
+              {item.imagePath && (<img src={item.imagePath} alt={item.title} className="event-card-image" />)}
+              {item.videoPath && <video className="event-card-image" controls src={item.videoPath}></video>}
               <div className="event-card-content">
                 <h3>{item.title}</h3>
                 <p className="event-time">{item.dateTime} - {item.uploadTime}</p>
