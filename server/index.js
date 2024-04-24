2f0f8daefccee3f6f135b7f0c8017c5678ee2e7a
Gld-Mx24483 <israeltoki7@gmail.com>
2024-04-24 03:39:24 +0100

    j

--- server/index.js
+++ server/index.js
@@ -1,4 +1,45 @@
-// index.js
+// // index.js
+// const express = require('express');
+// const cors = require('cors');
+// const path = require('path');
+// const contentManagementAPI = require('./content-management-api');
+// const eventManagementAPI = require('./event-management-api');
+// const galleryAPI = require('./gallery-api');
+// const volunteerAPI = require('./volunteer-api');
+// const teamAPI = require('./team-api');
+// const contactAPI = require('./contact-api');
+
+// const app = express();
+
+// app.use("/", (req,res) => {
+//   res.send("Server is running.");
+// });
+
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+// app.use('/', contentManagementAPI);
+// app.use('/', eventManagementAPI);
+// app.use('/', galleryAPI);
+// app.use('/', volunteerAPI);
+// app.use('/', teamAPI);
+// app.use('/', contactAPI);
+// app.use(cors({
+//   origin: 'http://localhost:3000' 
+// }));
+
+// app.use(cors({
+//   origin: 'https://dmfc.vercel.app' // or ['https://dmfc.vercel.app', 'http://localhost:3000'] for local development
+// }));
+
+
+// const port = process.env.PORT || 9000;
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
+
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
@@ -15,7 +56,15 @@ app.use("/", (req,res) => {
   res.send("Server is running.");
 });
 
-app.use(cors());
+const corsOptions = {
+  origin: ['https://dmfc.vercel.app', 'http://localhost:3000'], // Replace with your React app's URL(s)
+  optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true, // If you want to send cookies over CORS
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
@@ -23,16 +72,9 @@ app.use('/', contentManagementAPI);
 app.use('/', eventManagementAPI);
 app.use('/', galleryAPI);
 app.use('/', volunteerAPI);
-app.use('/', teamAPI);
+// app.use('/', teamAPI);
+app.use('/', teamAPI(corsOptions)); 
 app.use('/', contactAPI);
-app.use(cors({
-  origin: 'http://localhost:3000' 
-}));
-
-app.use(cors({
-  origin: 'https://dmfc.vercel.app' // or ['https://dmfc.vercel.app', 'http://localhost:3000'] for local development
-}));
-
 
 const port = process.env.PORT || 9000;
 app.listen(port, () => {
--- server/team-api.js
+++ server/team-api.js
@@ -1,4 +1,143 @@
-// team-api.js
+// // team-api.js
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const mysql = require('mysql');
+
+// const router = express.Router();
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'Golden m@trix24483',
+//   database: 'dmf_db'
+// });
+
+// connection.connect((err) => {
+//   if (err) {
+//     console.error('Error connecting to MySQL database:', err);
+//     return;
+//   }
+//   console.log('Team Connected to MySQL database');
+// });
+
+// router.put('/submit-team-form', (req, res) => {
+//     const { fullName, address, phoneNumber, email, role } = req.body;
+  
+//     const query = 'INSERT INTO team (fullName, address, phoneNumber, email, role, status) VALUES (?, ?, ?, ?, ?, ?)';
+//     connection.query(query, [fullName, address, phoneNumber, email, role, 'pending'], (error, results) => {
+//       if (error) {
+//         console.error('Error executing SQL query:', error);
+//         res.status(500).json({ error: 'Error submitting form' });
+//       } else {
+//         const insertedId = results.insertId;
+//         res.status(200).json({ message: 'Form submitted successfully', insertedId });
+//       }
+//     });
+//   });
+
+//   // Update user status to 'accepted'
+// router.post('/accept-request/:id', (req, res) => {
+//     const userId = req.params.id;
+  
+//     if (!userId || isNaN(userId)) {
+//       res.status(400).json({ error: 'Invalid user ID' });
+//       return;
+//     }
+  
+//     const query = 'UPDATE team SET status = "accepted" WHERE id = ?';
+//     connection.query(query, [userId], (error, results) => {
+//       if (error) {
+//         console.error('Error executing SQL query:', error);
+//         res.status(500).json({ error: 'Error accepting request' });
+//       } else {
+//         res.status(200).json({ message: 'Request accepted successfully' });
+//       }
+//     });
+//   });
+
+// // Get all team members based on status
+// // router.get('/get-team-members', (req, res) => {
+//   router.get('/get-team-members', cors(corsOptions), (req, res) => {
+//   const status = req.query.status;
+//   const email = req.query.email;
+//   let query = 'SELECT id, fullName, email, address, phoneNumber, role, createdAt FROM team';
+//   let whereClause = '';
+
+//   if (status === 'pending') {
+//     whereClause = ' WHERE status = "pending"';
+//   } else if (status === 'accepted') {
+//     whereClause = ' WHERE status = "accepted"';
+//   }
+
+//   if (email) {
+//     if (whereClause) {
+//       whereClause += ` AND email = '${email}'`;
+//     } else {
+//       whereClause = ` WHERE email = '${email}'`;
+//     }
+//   }
+
+//   query += whereClause;
+
+//   connection.query(query, (error, results) => {
+//     if (error) {
+//       console.error('Error executing SQL query:', error);
+//       res.status(500).json({ error: 'Error fetching team members' });
+//     } else {
+//       res.status(200).json(results);
+//     }
+//   });
+// });
+
+// // Delete a team member
+// router.delete('/delete-team-member/:id', (req, res) => {
+//     const userId = req.params.id;
+  
+//     if (!userId || isNaN(userId)) {
+//       res.status(400).json({ error: 'Invalid user ID' });
+//       return;
+//     }
+  
+//     const query = 'DELETE FROM team WHERE id = ?';
+//     connection.query(query, [userId], (error, results) => {
+//       if (error) {
+//         console.error('Error executing SQL query:', error);
+//         res.status(500).json({ error: 'Error deleting team member' });
+//       } else {
+//         res.status(200).json({ message: 'Team member deleted successfully' });
+//       }
+//     });
+//   });
+
+//   // Delete a pending team member
+// router.delete('/reject-request/:id', (req, res) => {
+//   const userId = req.params.id;
+
+//   if (!userId || isNaN(userId)) {
+//     res.status(400).json({ error: 'Invalid user ID' });
+//     return;
+//   }
+
+//   const query = 'DELETE FROM team WHERE id = ? AND status = "pending"';
+//   connection.query(query, [userId], (error, results) => {
+//     if (error) {
+//       console.error('Error executing SQL query:', error);
+//       res.status(500).json({ error: 'Error rejecting request' });
+//     } else {
+//       res.status(200).json({ message: 'Request rejected successfully' });
+//     }
+//   });
+// });
+
+// module.exports = router;
+
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -7,28 +146,30 @@ const mysql = require('mysql');
 const router = express.Router();
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+module.exports = (corsOptions) => {
+  router.use(cors(corsOptions));
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Golden m@trix24483',
-  database: 'dmf_db'
-});
+  app.use(express.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
 
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to MySQL database:', err);
-    return;
-  }
-  console.log('Team Connected to MySQL database');
-});
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Golden m@trix24483',
+    database: 'dmf_db'
+  });
 
-router.put('/submit-team-form', (req, res) => {
+  connection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to MySQL database:', err);
+      return;
+    }
+    console.log('Team Connected to MySQL database');
+  });
+
+  router.put('/submit-team-form', (req, res) => {
     const { fullName, address, phoneNumber, email, role } = req.body;
-  
+
     const query = 'INSERT INTO team (fullName, address, phoneNumber, email, role, status) VALUES (?, ?, ?, ?, ?, ?)';
     connection.query(query, [fullName, address, phoneNumber, email, role, 'pending'], (error, results) => {
       if (error) {
@@ -42,14 +183,14 @@ router.put('/submit-team-form', (req, res) => {
   });
 
   // Update user status to 'accepted'
-router.post('/accept-request/:id', (req, res) => {
+  router.post('/accept-request/:id', (req, res) => {
     const userId = req.params.id;
-  
+
     if (!userId || isNaN(userId)) {
       res.status(400).json({ error: 'Invalid user ID' });
       return;
     }
-  
+
     const query = 'UPDATE team SET status = "accepted" WHERE id = ?';
     connection.query(query, [userId], (error, results) => {
       if (error) {
@@ -61,48 +202,48 @@ router.post('/accept-request/:id', (req, res) => {
     });
   });
 
-// Get all team members based on status
-router.get('/get-team-members', (req, res) => {
-  const status = req.query.status;
-  const email = req.query.email;
-  let query = 'SELECT id, fullName, email, address, phoneNumber, role, createdAt FROM team';
-  let whereClause = '';
+  // Get all team members based on status
+  router.get('/get-team-members', (req, res) => {
+    const status = req.query.status;
+    const email = req.query.email;
+    let query = 'SELECT id, fullName, email, address, phoneNumber, role, createdAt FROM team';
+    let whereClause = '';
 
-  if (status === 'pending') {
-    whereClause = ' WHERE status = "pending"';
-  } else if (status === 'accepted') {
-    whereClause = ' WHERE status = "accepted"';
-  }
-
-  if (email) {
-    if (whereClause) {
-      whereClause += ` AND email = '${email}'`;
-    } else {
-      whereClause = ` WHERE email = '${email}'`;
+    if (status === 'pending') {
+      whereClause = ' WHERE status = "pending"';
+    } else if (status === 'accepted') {
+      whereClause = ' WHERE status = "accepted"';
     }
-  }
 
-  query += whereClause;
-
-  connection.query(query, (error, results) => {
-    if (error) {
-      console.error('Error executing SQL query:', error);
-      res.status(500).json({ error: 'Error fetching team members' });
-    } else {
-      res.status(200).json(results);
+    if (email) {
+      if (whereClause) {
+        whereClause += ` AND email = '${email}'`;
+      } else {
+        whereClause = ` WHERE email = '${email}'`;
+      }
     }
+
+    query += whereClause;
+
+    connection.query(query, (error, results) => {
+      if (error) {
+        console.error('Error executing SQL query:', error);
+        res.status(500).json({ error: 'Error fetching team members' });
+      } else {
+        res.status(200).json(results);
+      }
+    });
   });
-});
 
-// Delete a team member
-router.delete('/delete-team-member/:id', (req, res) => {
+  // Delete a team member
+  router.delete('/delete-team-member/:id', (req, res) => {
     const userId = req.params.id;
-  
+
     if (!userId || isNaN(userId)) {
       res.status(400).json({ error: 'Invalid user ID' });
       return;
     }
-  
+
     const query = 'DELETE FROM team WHERE id = ?';
     connection.query(query, [userId], (error, results) => {
       if (error) {
@@ -115,23 +256,24 @@ router.delete('/delete-team-member/:id', (req, res) => {
   });
 
   // Delete a pending team member
-router.delete('/reject-request/:id', (req, res) => {
-  const userId = req.params.id;
+  router.delete('/reject-request/:id', (req, res) => {
+    const userId = req.params.id;
 
-  if (!userId || isNaN(userId)) {
-    res.status(400).json({ error: 'Invalid user ID' });
-    return;
-  }
-
-  const query = 'DELETE FROM team WHERE id = ? AND status = "pending"';
-  connection.query(query, [userId], (error, results) => {
-    if (error) {
-      console.error('Error executing SQL query:', error);
-      res.status(500).json({ error: 'Error rejecting request' });
-    } else {
-      res.status(200).json({ message: 'Request rejected successfully' });
+    if (!userId || isNaN(userId)) {
+      res.status(400).json({ error: 'Invalid user ID' });
+      return;
     }
-  });
-});
 
-module.exports = router;
+    const query = 'DELETE FROM team WHERE id = ? AND status = "pending"';
+    connection.query(query, [userId], (error, results) => {
+      if (error) {
+        console.error('Error executing SQL query:', error);
+        res.status(500).json({ error: 'Error rejecting request' });
+      } else {
+        res.status(200).json({ message: 'Request rejected successfully' });
+      }
+    });
+  });
+
+  return router;
+};
