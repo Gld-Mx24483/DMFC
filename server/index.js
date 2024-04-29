3ba6092b3f26a8a7438a01ee1d587b7de17bfc8c
Gld-Mx24483 <israeltoki7@gmail.com>
2024-04-29 20:42:08 +0100

    dsa

--- server/index.js
+++ server/index.js
@@ -50,12 +50,11 @@ const eventManagementAPI = require('./event-management-api');
 const app = express();
 
 app.use(cors());
-app.use(express.json({ limit: '100000mb' }));
-app.use(express.urlencoded({ limit: '100000mb', extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.urlencoded({ extended: true, limit: '100000mb' })); // Updated line
-app.use(bodyParser.json({ limit: '100000mb' })); // Updated line
 // app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 // app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.use('/', teamAPI);
